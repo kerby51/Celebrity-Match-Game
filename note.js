1cb9f8a5c7e12ba67393656ccbd980025f2655d3
katharine benefico <kbenefico@gmail.com>
2016-09-01 11:15:47 -0400

    start connecting js to html/css

---
+++ note.js
@@ -0,0 +1,44 @@
+function flyingMusicNotes(numNotes) {
+
+  let $note;
+  const $body = $('body');
+
+  for (let i = 0; i < numNotes; i++) {
+    $note = $('<h1>').addClass('note').html('&#9834');
+    $body.append($note);
+
+    $note.animate({
+      top: Math.floor(Math.random() * 200) - 50 + '%',
+      left: Math.floor(Math.random() * 200) - 50 + '%',
+      opacity: 0,
+    }, 2000, 'linear');
+  }
+}
+
+setInterval(() => {
+  flyingMusicNotes(10);
+}, 2000);
+flyingMusicNotes(10);
+
+// function flyingMusicNotes(numNotes) {
+
+//   let $note;
+//   const $header = $('header');
+
+//   for (let i = 0; i < numNotes; i++) {
+//     $note = $('<h1>').addClass('note').html('&#9834');
+//     $header.append($note);
+
+//     $note.animate({
+//       top: Math.floor(Math.random() * 200) - 50 + '%',
+//       left: Math.floor(Math.random() * 200) - 50 + '%',
+//       opacity: 0,
+//     }, 3000, 'linear');
+//   }
+// }
+
+// setInterval(() => {
+//   flyingMusicNotes(10);
+// }, 3000);
+// flyingMusicNotes(10);
+
