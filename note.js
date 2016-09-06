0224056b2564ff81c2fc260d2c35dde6fb6a5474
katharine benefico <kbenefico@gmail.com>
2016-09-06 08:32:43 -0400

    clean up staging area

--- note.js
+++ note.js
@@ -20,25 +20,5 @@ setInterval(() => {
 }, 2000);
 flyingMusicNotes(10);
 
-// function flyingMusicNotes(numNotes) {
 
-//   let $note;
-//   const $header = $('header');
-
-//   for (let i = 0; i < numNotes; i++) {
-//     $note = $('<h1>').addClass('note').html('&#9834');
-//     $header.append($note);
-
-//     $note.animate({
-//       top: Math.floor(Math.random() * 200) - 50 + '%',
-//       left: Math.floor(Math.random() * 200) - 50 + '%',
-//       opacity: 0,
-//     }, 3000, 'linear');
-//   }
-// }
-
-// setInterval(() => {
-//   flyingMusicNotes(10);
-// }, 3000);
-// flyingMusicNotes(10);
 
